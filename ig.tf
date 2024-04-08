d55d3f8038bd05bf3976dd8f33cf9f80a68947d3
vijayaathavale <vijayas.athavale@gmail.com>
2024-04-08 22:44:15 +0200

    WordpressDocker project

---
+++ ig.tf
@@ -0,0 +1,37 @@
+#internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "upgrad-igw"
+  }
+}
+
+
+#public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+#private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
